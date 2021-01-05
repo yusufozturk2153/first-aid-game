1af35aa68d2f560d37f2ab67f8685bd854efe2e2
yusufozturk <yusufozturk2153@users.noreply.github.com>
2021-01-06 00:55:26 +0300

    Big Commit Details In Descreption
    Additions;
    -Player movement and animations
    -Injured man animations
    -Interactable objects and scripts
    -Game play instructions
    -Contents of examine for injured man (will change)

---
+++ First Aid/Assets/Scripts/GameLoader.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoader : MonoBehaviour
+{
+    //public Animator transition;
+    public float transitionTime = 1f;
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+             LoadGame();
+        }
+        else if (SceneManager.GetActiveScene().name=="Accident Scene")
+        {
+            LoadGame();
+        }
+      
+        
+    }
+
+    public void LoadGame()
+    {
+        StartCoroutine(LoadGame(SceneManager.GetActiveScene().buildIndex + 1));
+     
+    }
+
+    IEnumerator LoadGame(int gameIndex)
+    {
+        //Play Animatipn
+       //transition.SetTrigger("Start");
+        //Wait
+
+
+        yield return new WaitForSeconds(transitionTime);
+        //Load Game
+        SceneManager.LoadScene(gameIndex);
+    }
+}
---
+++ First Aid/Assets/Scripts/MovementAnimations.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAnimations : MonoBehaviour
+{
+    Animator animator;
+
+
+    [SerializeField] GameObject firstPersonCamera;
+    [SerializeField] float delay;
+    public bool isDriving;
+    
+    void Start()
+    {
+        animator=GetComponent<Animator>();
+        isDriving = true;
+        delay = 4f;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WalkOrRun();
+        GetOffTheCar();
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isDriving)
+        {
+            Invoke("ChangeCameraPosition", delay);
+            
+            
+        }
+    }
+
+
+    //Control walking and running animation
+
+    private void WalkOrRun()
+    {
+        //TODO create running controller
+
+        if (Input.GetKey("w"))
+        {
+           
+            animator.SetBool("isWalking", true);
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+    }
+
+    private void GetOffTheCar()
+    {
+        if (isDriving)
+        {
+            if (Input.GetKey("f"))
+            {
+                animator.SetBool("isWalking", true);
+                isDriving = false;
+            }
+        }
+        
+
+    }
+    void ChangeCameraPosition()
+    {
+        firstPersonCamera.transform.localPosition = new Vector3(0f, 3.4f, 0.2f);
+        GetComponent<CharacterController>().enabled = true;
+    }
+}
