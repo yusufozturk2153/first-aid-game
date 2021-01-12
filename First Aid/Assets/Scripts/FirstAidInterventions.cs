10ddd93b170c3e09de3b0045a8dc712c7bd5eeb4
yusufozturk <yusufozturk2153@users.noreply.github.com>
2021-01-13 01:51:28 +0300

    Examine Panel And Intervention Panel
    Intervention panel's buttons are static for now

---
+++ First Aid/Assets/Scripts/FirstAidInterventions.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class FirstAidInterventions : MonoBehaviour
+{
+    [SerializeField] GameObject examine;
+    [SerializeField] GameObject intervention;
+    [SerializeField] TextMeshProUGUI[] textsOnExaminePanel;
+
+    Button[] buttonsOnInterventionPanel;
+    void Start()
+    {
+        buttonsOnInterventionPanel = intervention.GetComponentsInChildren<Button>();
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void ShowExamine()
+    {
+        examine.SetActive(true);
+        ChangeTextsOnExamine();
+    }
+
+    public void ChangeTextsOnExamine()
+    {
+        string[] texts = new string[] { "The heart beat stopped", "Bleeding in the left arm", "Broken left foot" };
+
+        for(int i=0; i< textsOnExaminePanel.Length; i++)
+        {
+            textsOnExaminePanel[i].text = texts[i];
+        }
+    
+    }
+}
